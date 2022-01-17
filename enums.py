2bb973361d8245c4fcc6ea78edc4e0bc163c34df
R. Jay Mashl <rmashl@wustl.edu>
2022-01-17 09:44:06 -0600

    Supply missing file

---
+++ enums.py
@@ -0,0 +1,13 @@
+
+# R. Jay Mashl <rmashl@wustl.edu>
+
+# fusion filetypes
+SINGLE     = 1    # single-sample report
+COMBINED   = 2    # aggregated report
+
+# variant classes
+MUTATION   = 1
+FUSION     = 2
+
+# global
+UNDECLARED = 0
