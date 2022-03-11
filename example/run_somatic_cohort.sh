1b9f037097d0a8dc53bb9534ca24e83b6b31a78b
R. Jay Mashl <rmashl@wustl.edu>
2022-03-10 21:22:12 -0600

    Provide basic example script

---
+++ example/run_somatic_cohort.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# main script
+DRUGGABILITY=../druggability.py
+
+# Provide the location of a test maf; here, this is a cancer cohort
+cohort_maf=../Test_files/LUAD.Somatic.050919.mnp.annot.maf
+
+
+# Get samples
+cut -f16,17 $cohort_maf | sort -u | grep -v -i barcode > case_pairs.tsv
+
+cat case_pairs.tsv | while read line ; do
+    read tumor_sample normal_sample <<< $(echo "$line" | cut -d$'\t' -f 1,2)
+    OUTDIR=run_somatic_cohort_output
+    mkdir -p $OUTDIR
+    fileout_stem=$(basename $cohort_maf).${tumor_sample}_${normal_sample}
+    echo $fileout_stem
+    $DRUGGABILITY  -t maf  -f  $cohort_maf  -nn ${normal_sample} -tn ${tumor_sample}  -l $OUTDIR/$fileout_stem.log  -o $OUTDIR/$fileout_stem.out
+done
